a336aa8fee2f6fbb49135b39115d1b898d7f0b7d
Daniel Reyes <danieljosereyes0000@gmail.com>
2022-08-11 17:10:20 -0400

    Comienzo

--- src/App.js
+++ src/App.js
@@ -7,16 +7,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Repositorio del proyecto React js para coderhouse.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/danieljosereyes/flamenco"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link al Repositorio
         </a>
+        <p>
+          Proyecto FLAMENCO
+        </p>
       </header>
     </div>
   );
