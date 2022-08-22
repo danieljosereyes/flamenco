4bdd4899062ed14b6239446ab5ddf6f64844a044
Daniel Reyes <danieljosereyes0000@gmail.com>
2022-08-22 17:29:49 -0400

    [ADD] HEADER

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Repositorio del proyecto React js para coderhouse.
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/danieljosereyes/flamenco"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link al Repositorio
-        </a>
-        <p>
-          Proyecto FLAMENCO
-        </p>
-      </header>
+    <div className='header'
+    >
+        <a>Flamenco</a>
+        <NavBar/>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+
+
+const NavBar = () => {
+    return (        
+    <nav>
+        <a>Pasillo</a>
+        <a>Pedidos</a>
+        <a>Perfil</a>
+    </nav>
+    )
+}
+
+export default NavBar;
